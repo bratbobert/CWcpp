90b75c9131f9b62f9008bba6fca63aa5011b7a6a
bratbobert <101482794+bratbobert@users.noreply.github.com>
2024-01-16 22:50:39 +0300

    completed

--- 15.01.2024/defaultclass.cpp
+++ 15.01.2024/defaultclass.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 using namespace std;
 
-/*создать класс Дробь
- Объект данного класса должен состоять из числителя
- и знаменателя.
- Реализовать метод вывода дроби в виде:
- 3/6
- */
 class Drob {
 private:
 	int num;
 	int denum;
 
-public: 
+public:
 	Drob(int num, int denum) {
 		this->num = num;
 		this->denum = denum;
@@ -24,34 +18,34 @@ public:
 
 };
 
-//class Stb {
-//private:
-//	string grp;
-//	string name;
-//	float avg;
-//
-//public:
-//	Stb(string grp, string name, float avg) {
-//		this->grp = grp;
-//		this->name = name;
-//		this->avg = avg
-//	}
-//	void show() {
-//		cout << grp << " " << name << avg;
-//	}
-//
-//	string getStatus() {
-//		if (avg > 2.8) return "ABC";
-//		else return "CBA";
-//	}
-//}; fix in home
+class Stb {
+private:
+	string grp;
+	string name;
+	float avg;
+
+public:
+	Stb(string grp, string name, float avg) {
+		this->grp = grp;
+		this->name = name;
+		this->avg = avg;
+	}
+	void show() {
+		cout << grp << " " << name << avg;
+	}
+
+	string getStatus() {
+		if (avg > 2.8) return "ABC";
+		else return "CBA";
+	}
+}; 
 
 class Brod {
 private:
 	int num;
 	int denum;
 	int total = 0;
-	
+
 public:
 	Brod(int num, int denum) {
 		total = num / denum;
@@ -67,13 +61,13 @@ public:
 
 int main() {
 
-	/*Drob num(2, 5);
-	num1.show();*/
-
-	/*Stb st1("25", "Maga", 2.8);
+	Drob figure(2, 5);
+	figure.show();
+	cout << endl;
+	
+	Stb st1("25", "Maga", 2.8);
 	st1.show();
 	cout << st1.getStatus() << endl;
-	*/
 	
 	Brod num1(5, 2);
 	Brod num2(2, 5);
